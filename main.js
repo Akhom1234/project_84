b7bdd8bf41b517ff5fda332f5ca0afc7f2fdfcb7
Akhom1234 <98893867+Akhom1234@users.noreply.github.com>
2022-04-18 20:29:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-pizza_list_array = [];
+ppizza_list_array = [];
 
 function getmenu()
 	{    
-
-        var item = document.getElementById(" pizza_name").value; 
-       
-  
-        pizza_list_array. push()  (item);
+        var item = document.getElementById("pizza_name").value; 
+        pizza_list_array.push(item);
         
         var remove_commas = pizza_list_array.join("<br># ");
         document.getElementById("display_menu").innerHTML =  "# " + remove_commas;
